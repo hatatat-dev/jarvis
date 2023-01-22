9f9a132f854107bec69145dc0d3b5f2fd14afef6
hatatat-dev <59328234+hatatat-dev@users.noreply.github.com>
2023-01-21 17:01:55 -0800

    add partner controller

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ brain=Brain()
 
 # Robot configuration code
 controller_1 = Controller(PRIMARY)
+controller_2 = Controller(PARTNER)
 left_motor_a = Motor(Ports.PORT20, GearSetting.RATIO_18_1, False)
 left_motor_b = Motor(Ports.PORT10, GearSetting.RATIO_18_1, False)
 left_drive_smart = MotorGroup(left_motor_a, left_motor_b)
@@ -124,14 +125,105 @@ def rc_auto_loop_function_controller_1():
         # wait before repeating the process
         wait(20, MSEC)
 
+# define variables used for controlling motors based on controller inputs
+controller_2_left_shoulder_control_motors_stopped = True
+controller_2_right_shoulder_control_motors_stopped = True
+drivetrain_l_needs_to_be_stopped_controller_2 = False
+drivetrain_r_needs_to_be_stopped_controller_2 = False
+
+# define a task that will handle monitoring inputs from controller_2
+def rc_auto_loop_function_controller_2():
+    global drivetrain_l_needs_to_be_stopped_controller_2, drivetrain_r_needs_to_be_stopped_controller_2, controller_2_left_shoulder_control_motors_stopped, controller_2_right_shoulder_control_motors_stopped, remote_control_code_enabled
+    # process the controller input every 20 milliseconds
+    # update the motors based on the input values
+    while True:
+        if remote_control_code_enabled:
+            # stop the motors if the brain is calibrating
+            if drivetrain_inertial.is_calibrating():
+                left_drive_smart.stop()
+                right_drive_smart.stop()
+                while drivetrain_inertial.is_calibrating():
+                    sleep(25, MSEC)
+            
+            # calculate the drivetrain motor velocities from the controller joystick axies
+            # left = axis3
+            # right = axis2
+            drivetrain_left_side_speed = controller_2.axis3.position()
+            drivetrain_right_side_speed = controller_2.axis2.position()
+            
+            # check if the value is inside of the deadband range
+            if drivetrain_left_side_speed < 5 and drivetrain_left_side_speed > -5:
+                # check if the left motor has already been stopped
+                if drivetrain_l_needs_to_be_stopped_controller_2:
+                    # stop the left drive motor
+                    left_drive_smart.stop()
+                    # tell the code that the left motor has been stopped
+                    drivetrain_l_needs_to_be_stopped_controller_2 = False
+            else:
+                # reset the toggle so that the deadband code knows to stop the left motor next
+                # time the input is in the deadband range
+                drivetrain_l_needs_to_be_stopped_controller_2 = True
+            # check if the value is inside of the deadband range
+            if drivetrain_right_side_speed < 5 and drivetrain_right_side_speed > -5:
+                # check if the right motor has already been stopped
+                if drivetrain_r_needs_to_be_stopped_controller_2:
+                    # stop the right drive motor
+                    right_drive_smart.stop()
+                    # tell the code that the right motor has been stopped
+                    drivetrain_r_needs_to_be_stopped_controller_2 = False
+            else:
+                # reset the toggle so that the deadband code knows to stop the right motor next
+                # time the input is in the deadband range
+                drivetrain_r_needs_to_be_stopped_controller_2 = True
+            
+            # only tell the left drive motor to spin if the values are not in the deadband range
+            if drivetrain_l_needs_to_be_stopped_controller_2:
+                left_drive_smart.set_velocity(drivetrain_left_side_speed, PERCENT)
+                left_drive_smart.spin(FORWARD)
+            # only tell the right drive motor to spin if the values are not in the deadband range
+            if drivetrain_r_needs_to_be_stopped_controller_2:
+                right_drive_smart.set_velocity(drivetrain_right_side_speed, PERCENT)
+                right_drive_smart.spin(FORWARD)
+            # check the buttonL1/buttonL2 status
+            # to control Intake
+            if controller_2.buttonL1.pressing():
+                Intake.spin(FORWARD)
+                controller_2_left_shoulder_control_motors_stopped = False
+            elif controller_2.buttonL2.pressing():
+                Intake.spin(REVERSE)
+                controller_2_left_shoulder_control_motors_stopped = False
+            elif not controller_2_left_shoulder_control_motors_stopped:
+                Intake.stop()
+                # set the toggle so that we don't constantly tell the motor to stop when
+                # the buttons are released
+                controller_2_left_shoulder_control_motors_stopped = True
+            # check the buttonR1/buttonR2 status
+            # to control Flywheel
+            if controller_2.buttonR1.pressing():
+                Flywheel.spin(FORWARD)
+                controller_2_right_shoulder_control_motors_stopped = False
+            elif controller_2.buttonR2.pressing():
+                Flywheel.spin(REVERSE)
+                controller_2_right_shoulder_control_motors_stopped = False
+            elif not controller_2_right_shoulder_control_motors_stopped:
+                Flywheel.stop()
+                # set the toggle so that we don't constantly tell the motor to stop when
+                # the buttons are released
+                controller_2_right_shoulder_control_motors_stopped = True
+        # wait before repeating the process
+        wait(20, MSEC)
+
+
 # define variable for remote controller enable/disable
 remote_control_code_enabled = True
 
 rc_auto_loop_thread_controller_1 = Thread(rc_auto_loop_function_controller_1)
+rc_auto_loop_thread_controller_2 = Thread(rc_auto_loop_function_controller_2)
 #endregion VEXcode Generated Robot Configuration
 vexcode_brain_precision = 0
 vexcode_console_precision = 0
 vexcode_controller_1_precision = 0
+vexcode_controller_2_precision = 0
 myVariable = 0
 test = 0
 message1 = Event()
